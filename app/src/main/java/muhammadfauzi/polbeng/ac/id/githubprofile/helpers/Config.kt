6945b6d63b7d024faf132bbff35439d9b82d52f4
ACER <m.fauzi30052004@gmail.com>
2024-12-09 14:30:29 +0700

    add Config.kt

---
+++ app/src/main/java/muhammadfauzi/polbeng/ac/id/githubprofile/helpers/Config.kt
@@ -0,0 +1,27 @@
+package muhammadfauzi.polbeng.ac.id.githubprofile.helpers
+import de.hdodenhof.circleimageview.BuildConfig
+
+class Config {
+    companion object {
+        const val SPLASH_SCREEN_DELAY: Long = 3000
+        const val BASE_URL = "https://api.github.com"
+        const val DEFAULT_USER_LOGIN = "Fauzi326"
+        //const val PERSONAL_ACCESS_TOKEN = "token ${BuildConfig.ACCESS_TOKEN}"
+    }
+}
+//package muhammadfauzi.polbeng.ac.id.githubprofile.helpers
+//
+//import de.hdodenhof.circleimageview.BuildConfig
+//import muhammadfauzi.polbeng.ac.id.githubprofile.BuildConfig
+//
+//class Config {
+//    companion object {
+//        const val SPLASH_SCREEN_DELAY: Long = 3000 // Delay splash screen dalam milidetik
+//        const val BASE_URL: String = "https://api.github.com" // URL dasar API GitHub
+//        const val DEFAULT_USER_LOGIN: String = "Fauzi326" // Username default untuk pencarian
+//        const val PERSONAL_ACCESS_TOKEN: String = "token ${BuildConfig.ACCESS_TOKEN}" // Token akses pribadi
+//
+//        }
+//
+//    }
+//}
